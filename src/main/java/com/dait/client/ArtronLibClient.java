5cbb47bf2d0b003b9c23969b8de644abb21e8068
Loqor <isaiahcrace@gmail.com>
2025-07-20 14:19:02 -0500

    I wanna play the game too

--- src/main/java/com/dait/client/ArtronLibClient.java
+++ src/main/java/com/dait/client/ArtronLibClient.java
@@ -6,10 +6,6 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 public class ArtronLibClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ClientPlayConnectionEvents.JOIN.register((clientPlayNetworkHandler, packetSender, minecraftClient) -> {
-            if(minecraftClient.player.getUuidAsString().equalsIgnoreCase("ad504e7c-22a0-4b3f-94e3-5b6ad5514cb6")) {
-                minecraftClient.close();
-            }
-        });
+
     }
 }
